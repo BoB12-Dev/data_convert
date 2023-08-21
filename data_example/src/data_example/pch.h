3f616568109416f9dc7f9c6d470d8ea48def3f59
2W <psh0036@suwon.ac.kr>
2023-08-21 23:29:46 +0900

    Add files via upload

---
+++ data_example/src/data_example/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include "../../../cppcore/Inc/cppcore.h"
+#include "report.h"
+
+#include <string>
+#include <list>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace core;
+
+#pragma comment(lib, "cppcore.lib")
